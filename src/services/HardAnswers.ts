0a1a4c8d9518a53f448f73d8af0dbb4035ac5d6c
maurice <maurice.mason97@gmail.com>
2022-06-24 16:49:23 -0400

    difficulty

--- src/services/HardAnswers.ts
+++ src/services/HardAnswers.ts
@@ -21,3 +21,215 @@ export const hardQOne = [
   "unknown",
   "shadow",
 ];
+const hardPokemon = [
+  {
+    name: "venusaur",
+    strengths: "",
+    weaknesses: "fire",
+    description:
+      "Its plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight.",
+  },
+  {
+    name: "charizard",
+    strengths: "",
+    weaknesses: "water",
+    description:
+      "It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.",
+  },
+  {
+    name: "blastoise",
+    strengths: "",
+    weaknesses: "grass",
+    description:
+      "It crushes its foe under its heavy body to cause fainting. In a pinch, it will withdraw inside its shell.",
+  },
+  {
+    name: "butterfree",
+    strengths: "",
+    weaknesses: "fire",
+    description:
+      "In battle, it flaps its wings at great speed to release highly toxic dust into the air.",
+  },
+  {
+    name: "beedrill",
+    strengths: "",
+    weaknesses: "fire",
+    description:
+      "It has three poisonous stingers on its forelegs and its tail. They are used to jab its enemy repeatedly.",
+  },
+  {
+    name: "pidgeot",
+    strengths: "",
+    weaknesses: "electric",
+    description:
+      "This Pokémon flies at Mach 2 speed, seeking prey. Its large talons are feared as wicked weapons.",
+  },
+  {
+    name: "nidoqueen",
+    strengths: "",
+    weaknesses: "water",
+    description:
+      "It is better at defense than offense. With scales like armor, this Pokémon will shield its children from any kind of attack.",
+  },
+  {
+    name: "nidoking",
+    strengths: "",
+    weaknesses: "water",
+    description:
+      "When it goes on a rampage, it’s impossible to control. But in the presence of a female it’s lived with for a long time, it calms down.",
+  },
+  {
+    name: "vileplume",
+    strengths: "",
+    weaknesses: "fire",
+    description:
+      "It has the world’s largest petals. With every step, the petals shake out heavy clouds of toxic pollen.",
+  },
+  {
+    name: "poliwrath",
+    strengths: "",
+    weaknesses: "fairy",
+    description:
+      "Its body is solid muscle. When swimming through cold seas, it uses its impressive arms to smash through drift ice and plow forward.",
+  },
+  {
+    name: "alakazam",
+    strengths: "",
+    weaknesses: "ghost",
+    description:
+      "It has an incredibly high level of intelligence. Some say that it remembers everything that ever happens to it, from birth till death.",
+  },
+  {
+    name: "machamp",
+    strengths: "",
+    weaknesses: "psychic",
+    description:
+      "It quickly swings its four arms to rock its opponents with ceaseless punches and chops from all angles.",
+  },
+  {
+    name: "victreebell",
+    strengths: "",
+    weaknesses: "fire",
+    description:
+      "Lures prey with the sweet aroma of honey. Swallowed whole, the prey is dissolved in a day, bones and all.",
+  },
+  {
+    name: "golem",
+    strengths: "",
+    weaknesses: "fighting",
+    description:
+      "Once it sheds its skin, its body turns tender and whitish. Its hide hardens when it’s exposed to air.",
+  },
+  {
+    name: "gengar",
+    strengths: "",
+    weaknesses: "ghost",
+    description:
+      "On the night of a full moon, if shadows move on their own and laugh, it must be it’s doing.",
+  },
+  {
+    name: "hitmonlee",
+    strengths: "",
+    weaknesses: "psychic",
+    description:
+      "This amazing Pokémon has an awesome sense of balance. It can kick in succession from any position.",
+  },
+  {
+    name: "hitmonchan",
+    strengths: "",
+    weaknesses: "psychic",
+    description:
+      "Its punches slice the air. They are launched at such high speed, even a slight graze could cause a burn.",
+  },
+  {
+    name: "lapras",
+    strengths: "",
+    weaknesses: "grass",
+    description:
+      "A smart and kindhearted Pokémon, it glides across the surface of the sea while its beautiful song echoes around it.",
+  },
+  {
+    name: "eevee",
+    strengths: "",
+    weaknesses: "fighting",
+    description:
+      "It has the ability to alter the composition of its body to suit its surrounding environment.",
+  },
+  {
+    name: "vaporeon",
+    strengths: "",
+    weaknesses: "grass",
+    description:
+      "When it’s fins begin to vibrate, it is a sign that rain will come within a few hours.",
+  },
+  {
+    name: "jolteon",
+    strengths: "",
+    weaknesses: "ground",
+    description:
+      "If it is angered or startled, the fur all over its body bristles like sharp needles that pierce foes.",
+  },
+  {
+    name: "flareon",
+    strengths: "",
+    weaknesses: "water",
+    description:
+      "Once it has stored up enough heat, this Pokémon’s body temperature can reach up to 1,700 degrees Fahrenheit.",
+  },
+  {
+    name: "aerodactyl",
+    strengths: "",
+    weaknesses: "water",
+    description:
+      "This is a ferocious Pokémon from ancient times. Apparently even modern technology is incapable of producing a perfectly restored specimen.",
+  },
+  {
+    name: "snorlax",
+    strengths: "",
+    weaknesses: "fighting",
+    description:
+      "It is not satisfied unless it eats over 880 pounds of food every day. When it is done eating, it goes promptly to sleep.",
+  },
+  {
+    name: "articuno",
+    strengths: "",
+    weaknesses: "fire",
+    description:
+      "It’s said that this Pokémon’s beautiful blue wings are made of ice. It flies over snowy mountains, its long tail fluttering along behind it.",
+  },
+  {
+    name: "zapdos",
+    strengths: "",
+    weaknesses: "ice",
+    description:
+      "This Pokémon has complete control over electricity. There are tales of it nesting in the dark depths of pitch-black thunderclouds.",
+  },
+  {
+    name: "moltres",
+    strengths: "",
+    weaknesses: "water",
+    description:
+      "It’s one of the legendary bird Pokémon. When it flaps its flaming wings, they glimmer with a dazzling red glow.",
+  },
+  {
+    name: "dragonite",
+    strengths: "",
+    weaknesses: "fairy",
+    description:
+      "It’s a kindhearted Pokémon. If it spots a drowning person or Pokémon, it simply must help them.",
+  },
+  {
+    name: "mewtwo",
+    strengths: "",
+    weaknesses: "ghost",
+    description:
+      "Its DNA is almost the same as another pokemon’s. However, its size and disposition are vastly different.",
+  },
+  {
+    name: "mew",
+    strengths: "",
+    weaknesses: "ghost",
+    description:
+      "When viewed through a microscope, this Pokémon’s short, fine, delicate hair can be seen.",
+  },
+];
